4e419b3c0d8c169cefd5dc4d88157fa8730486ca
kentakom1213 <kentakom1213@gmail.com>
2026-05-18 11:58:57 +0900

    fix: simplify python server

--- python-app/app/main.py
+++ python-app/app/main.py
@@ -4,22 +4,17 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI"}
-
-
-@app.get("/hello", response_class=HTMLResponse)
-def hello():
+@app.get("/", response_class=HTMLResponse)
+def root():
     return """
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <title>FastAPI Server</title>
-      </head>
-      <body>
-        <h1>Hello from FastAPI</h1>
-      </body>
-    </html>
-    """
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>FastAPI Server</title>
+  </head>
+  <body>
+    <h1>Hello from FastAPI</h1>
+  </body>
+</html>
+"""
